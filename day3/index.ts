0fae8a10d55b9c470754a06b43e22961a2e99aed
Philip Pannenko <philip.pannenko@gmail.com>
2022-12-04 09:24:28 -0500

    day 3 part 2

--- day3/index.ts
+++ day3/index.ts
@@ -10,31 +10,53 @@ import readline from 'readline';
         input: fileStream,
         crlfDelay: Infinity,
     });
-    let i = 1;
+
     let sum = 0;
+
+    // used for part2
+    let groupIncrement = 0;
+    let groupBadgeCandidates = new Map<string, number>();
+
     for await (const line of rl) {
 
-        let bag1 = line.substring(0, line.length/2).split('');
-        let bag2 = line.substring(line.length/2).split('');
+        if (isPart1) {
 
-        let item = bag2.find( x => {
-            let foo = bag1.includes(x)
-            return foo;
-        }) || '';
+            let rucksack1 = line.substring(0, line.length / 2).split('');
+            let rucksack2 = line.substring(line.length / 2).split('');
 
-        let val = 0;
-        if( item === item.toLowerCase()) {
-            val = item.charCodeAt(0)-96;
+            let badge = rucksack2.find(x => rucksack1.includes(x)) || '';
+
+            // -96 and -64 are the ASCII offset; +26 is the puzzle's capital letter value offset
+            sum += badge.charCodeAt(0) + (badge === badge.toLowerCase() ? -96 : -64 + 26);
+
         } else {
-            val = item.charCodeAt(0)-64 + 26;
+
+            const rucksack = line.split('');
+            const rucksackBadgeCandidates = new Map<string, number>();
+
+            rucksack.forEach(x => {
+                rucksackBadgeCandidates.set(x, 1);
+            });
+
+            rucksackBadgeCandidates.forEach((v, k) => {
+                // @ts-ignore (https://github.com/microsoft/TypeScript/issues/9619)
+                groupBadgeCandidates.set(k, groupBadgeCandidates.has(k) ? groupBadgeCandidates.get(k) + v : v);
+            })
+
+            groupIncrement++;
+
+            if (groupIncrement >= 3) {
+                for (const [k, v] of groupBadgeCandidates.entries()) {
+                    if (v === 3) {
+                        // -96 and -64 are the ASCII offset; +26 is the puzzle's capital letter value offset
+                        sum += k.charCodeAt(0) + (k === k.toLowerCase() ? -96 : -64 + 26);
+                        break;
+                    }
+                }
+                groupIncrement = 0;
+                groupBadgeCandidates = new Map<string, number>();
+            }
         }
-
-        console.log(i++ + ' ' + item + ' ' + val);
-        // mconsole.log(item.charCodeAt(0) + '\n');
-
-        sum += val;
-
     }
-
     console.log(sum);
 })()
