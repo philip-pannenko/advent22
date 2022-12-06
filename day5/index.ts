d52c9202cdc538e8ad4f2fbff8f630408aa24d68
Philip Pannenko <philip.pannenko@gmail.com>
2022-12-05 20:02:02 -0500

    day 5 part a

---
+++ day5/index.ts
@@ -0,0 +1,82 @@
+import fs from 'fs';
+import readline from 'readline';
+
+(async () => {
+
+    const fileStream = fs.createReadStream('./day5/input.txt');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
+
+    let stack: Array<Array<string>> = [];
+    let isStackBuilt = false;
+    for await (const line of rl) {
+
+        // Break out of the stack/create generation loop once the last stack/crate line is read
+        if (line.length === 0) {
+            isStackBuilt = true;
+        }
+
+        // First build out the stack/crate data structure
+        else if (!isStackBuilt) {
+
+            // Create an empty row of stacks
+            if (stack.length === 0) {
+
+                // Note: +1 added to line length to account for non-zero based iteration.
+                for (let i = 0; i < (line.length / 4) + 1; i++) {
+                    stack.push([]);
+                }
+            }
+
+            let stackIterator = 0;
+
+            for (let i = 0; i < line.length; i++) {
+
+                // Begin to break out of the stack/create generation loop once the last stack/crate line is detected
+                if (line.charAt(1) === '1') {
+                    break;
+                }
+
+                // The format of each crate within is stack is a size of 4 characters
+                const crateIterator = i % 4;
+
+                if (crateIterator === 0) {
+                    // Increment the amount of stacks for this row once the crate is all done for the stack
+                    stackIterator++;
+                }
+
+                    // else if (crateIterator === 2 || crateIterator === 3) {
+                    // do nothing
+                // }
+
+                else if (crateIterator === 1) {
+                    // Only do processing on the stack if the crate exists
+                    if (line[i] !== ' ') {
+                        stack[stackIterator].unshift(line[i]);
+                    }
+                }
+            }
+        } else {
+
+            const [, quantity, , from, , to] = line.split(' ').map(Number);
+
+            for (let i = 0; i < quantity; i++) {
+                stack[to].push(stack[from].pop() || '');
+            }
+          
+        }
+
+    }
+
+    //Remove the zero-based empty array at index zero from the display; 
+    stack.shift();
+
+    console.log(stack.reduce(
+        (previousValue, currentValue) =>
+            previousValue + currentValue.pop()
+        , ''));
+
+})()
---
+++ day5/scratch.md
@@ -0,0 +1,22 @@
+```
+    
+    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+```
+
+first build out the storage
+```
+iterate over the line.
+for i++ ; i<line.length
+i%0 "["
+i%1 letter
+i%2 "]"
+i%3 iterate crate
+```
